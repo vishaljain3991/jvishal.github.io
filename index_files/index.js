f03ab87a67175bbce4302e653d169feb5853a3a7
vishaljain3991 <vishaljainthebest@gmail.com>
2016-10-29 10:58:50 +0530

    Initial Commit

---
+++ index_files/index.js
@@ -0,0 +1,9 @@
+// Created by iWeb 2.0.4 local-build-20111120
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('index_files/indexMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');fixAllIEPNGs('Media/transparent.gif');applyEffects()}
